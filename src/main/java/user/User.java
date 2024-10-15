4abb50d3740932a175a8141a55fad4ab9276670f
Rabiya Dar <rabiya_khalid@hotmail.com>
2024-10-15 15:54:05 +0200

    My first commit

---
+++ src/main/java/user/User.java
@@ -0,0 +1,21 @@
+package user;
+
+public class User {
+    // private datatype attributename
+    private String username, password;
+    public User(String username, String password) {
+        this.username= username;
+        this.password =  password;
+
+     //   return userName, passWord;
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
---
+++ src/test/java/testUser/TestUser.java
@@ -0,0 +1,35 @@
+package testUser;
+
+import org.junit.jupiter.api.Test;
+import user.User;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestUser {
+// skriva enhets klass
+@Test
+    public void testUser(){
+   // skapa ett objekt av klassen User
+    User myUser = new User("username", "password"); // röd först user men vi ska autogenerera
+    // genom att hovra på den, more actions: skapa user klass under project java, project user
+    // sen skriv två parametrar "username" och "password"
+    // hovra på dem och skapa med more actions, skapa constructor
+    // det ska inte vara en definiering d.v.s. String a, String b<-- fel
+
+String myUsername = myUser.getUsername();
+assertEquals("username", myUsername);
+}
+    @Test
+    public void testPassword(){
+        // skapa ett objekt av klassen User
+        User myUser = new User("username", "password"); // röd först user men vi ska autogenerera
+        // genom att hovra på den, more actions: skapa user klass under project java, project user
+        // sen skriv två parametrar "username" och "password"
+        // hovra på dem och skapa med more actions, skapa constructor
+        // det ska inte vara en definiering d.v.s. String a, String b<-- fel
+
+        String myPassword = myUser.getPassword();
+        assertEquals("password", myPassword);
+    }
+
+}
